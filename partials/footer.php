f17f8aba0b0fbe3da62e5ec6b5daac6d22928ca0
mickaelquint <nasribery13@gmail.com>
2019-03-04 12:23:13 +0100

    library

---
+++ partials/footer.php
@@ -0,0 +1,19 @@
+<!-- Footer -->
+<footer class="py-2 ">
+    <div class="container">
+      <p class="m-0 text-center text-dark">Copyright &copy;
+        <?php echo $siteName .' '. date('Y');?> </p>
+        <?php // echo (new DateTime())->format('Y');?>
+        
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ public/index.php
@@ -0,0 +1,48 @@
+<?php
+
+// On inclus le fichier header.php sur la page
+require_once __DIR__ . '/../partials/header.php';
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+// On inclus le fichier header.php sur la page
+require_once __DIR__ . '/../partials/footer.php';
+
+
+
+
+
+?>
+<!-- Bootstrap core JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
